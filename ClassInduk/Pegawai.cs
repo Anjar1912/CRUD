e18d5ed8b88ae036293a90640f54fd5a56430eed
Anjar1912 <61839743+Anjar1912@users.noreply.github.com>
2020-06-28 14:06:07 +0700

    Program CRUD

--- ClassInduk/Pegawai.cs
+++ ClassInduk/Pegawai.cs
@@ -1,1 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace AplikasiPegawai.ClassInduk
+{
+    public abstract class Pegawai
+    {
+        public string Nama { get; set; }
+        public string Nik { get; set; }
+        public string jenis_pegawai { get; set; }
+
+        public abstract int Gaji(); 
+    }
+}
