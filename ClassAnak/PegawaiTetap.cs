7dfa699cf14fb7979caf90b23770f9572b146941
Anjar1912 <61839743+Anjar1912@users.noreply.github.com>
2020-06-28 14:07:55 +0700

    Program CRUD

---
+++ ClassAnak/PegawaiTetap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AplikasiPegawai.ClassInduk;
+
+namespace AplikasiPegawai.ClassAnak
+{
+    public class PegawaiTetap : Pegawai
+    {
+        public int GajiBulanan { get; set; }
+
+        public override int Gaji() => GajiBulanan;
+    }
+}
